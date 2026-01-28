7a6f6328d00460fa3a45999bd915f204939c2267
Azeem Solangi <azeembscs86@gmail.com>
2026-01-28 17:56:33 +0500

    Delete src/controllers directory

--- src/controllers/echoController.ts
+++
@@ -1,8 +0,0 @@
-import { Request, Response } from "express";
-
-export const echoRequest = (request: Request, response: Response) => {
-  response.status(200).json({
-    message: "Request accepted",
-    payload: request.body
-  });
-};
